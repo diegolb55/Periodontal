80c47180184007eb78ba3e80e38f12742c69c0e7
kimchi55 <diegolugobendezu55>
2022-01-22 13:24:03 -0400

    tweaking

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -5,4 +5,32 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+$(function(){  // $(document).ready shorthand
+  $('.monster').fadeIn("fast");
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
